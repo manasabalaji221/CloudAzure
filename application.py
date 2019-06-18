31f5b66b9a899bee641c13bbcb237f35ecf4ee29
manasabalaji221 <manasbalaji4857@gmail.com>
2019-06-18 01:18:35 -0500

    added html and odbc

--- application.py
+++ application.py
@@ -1,6 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
+import pypyodbc
 app = Flask(__name__)
+connection = pypyodbc.connect("Driver={ODBC Driver 17 for SQL Server};Server=tcp:servermanasa.database.windows.net,1433;Database=database1;Uid=serverm;Pwd=BluDiam0@;")
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+@app.route('/')
+def hello_world():
+    cursor = connection.cursor()
+    cursor.execute("select count(*) from all_month")
+    rows = cursor.fetchall()
+    count = rows[0][0]
+    return render_template('index.html', count=count)
+
+
+if __name__ == '__main__':
+    app.run()
